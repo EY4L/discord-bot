3613bc18422aa870d8c444d227461a92727d3ca4
Eyal TH <eyaltornhibler@gmail.com>
2022-08-29 12:58:57 +0100

    Add command

--- develop/notebooks/main.py
+++ develop/notebooks/main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 import nest_asyncio
@@ -17,35 +18,61 @@ bot = discord.Client(intents=discord.Intents.default())
 
 
 # # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
-# @bot.event
-# async def on_ready():
-#     # CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
-#     guild_count = 0
+@bot.event
+async def on_ready():
+    # CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
+    guild_count = 0
 
-#     # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
-#     for guild in bot.guilds:
-#         # PRINT THE SERVER'S ID AND NAME.
-#         print(f"- {guild.id} (name: {guild.name})")
+    # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+    for guild in bot.guilds:
+        # PRINT THE SERVER'S ID AND NAME.
+        print(f"- {guild.id} (name: {guild.name})")
 
-#         # INCREMENTS THE GUILD COUNTER.
-#         guild_count = guild_count + 1
+        # INCREMENTS THE GUILD COUNTER.
+        guild_count = guild_count + 1
 
-#     # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
-#     print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+    # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
 
 
 # EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+# @bot.event
+# async def on_message(message):
+
+#     # If message contains 'hello testbot'
+#     if message.content == "hello testbot":
+
+#         # Send response
+#         await message.channel.send("Hello, got any Shmeckles?")
+
+
 @bot.event
 async def on_message(message):
-    # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-    if message.content == "hello testbot":
-        # SENDS BACK A MESSAGE TO THE CHANNEL.
-        await message.channel.send("Hello, got any Shmeckles?")
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
 
+    print(f"Message {user_message} by {username} on {channel}")
 
-@bot.event
-async def on_ready():
-    await print(f"{bot.user} has connected to Discord!")
+    if message.author == bot.user:
+        return
+
+    if channel == "general":
+        if user_message.lower() == "hello" or user_message.lower() == "hi":
+            await message.channel.send(f"Hello {username}")
+            return
+        elif user_message.lower() == "bye":
+            await message.channel.send(f"Bye {username}")
+        elif user_message.lower() == "tell me a joke":
+            jokes = [
+                " Can someone please shed more\
+            light on how my lamp got stolen?",
+                "Why is she called llene? She\
+                     stands on equal legs.",
+                "What do you call a gazelle in a \
+                     lions territory? Denzel.",
+            ]
+            await message.channel.send(random.choice(jokes))
 
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
