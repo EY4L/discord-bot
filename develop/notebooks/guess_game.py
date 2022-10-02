54491f7e56673da15f939564e98456a7664b148f
Eyal TH <eyaltornhibler@gmail.com>
2022-10-02 20:42:53 +0100

    Add 8ball function

--- develop/notebooks/guess_game.py
+++ develop/notebooks/guess_game.py
@@ -5,6 +5,7 @@ import random
 import discord
 import nest_asyncio
 from dotenv import find_dotenv, load_dotenv
+from eight_ball import eight_ball_answer
 
 nest_asyncio.apply()
 
@@ -20,8 +21,11 @@ class MyClient(discord.Client):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
         print("------")
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
+    async def on_message(
+        self,
+        message,
+    ):
+        # We do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
 
@@ -31,10 +35,17 @@ class MyClient(discord.Client):
             def is_correct(m):
                 return m.author == message.author and m.content.isdigit()
 
-            answer = random.randint(1, 10)
+            answer = random.randint(
+                1,
+                10,
+            )
 
             try:
-                guess = await self.wait_for("message", check=is_correct, timeout=5.0)
+                guess = await self.wait_for(
+                    "message",
+                    check=is_correct,
+                    timeout=5.0,
+                )
             except asyncio.TimeoutError:
                 return await message.channel.send(
                     f"Sorry, you took too long it was {answer}."
@@ -44,6 +55,9 @@ class MyClient(discord.Client):
                 await message.channel.send("You are right!")
             else:
                 await message.channel.send(f"Oops. It is actually {answer}.")
+
+        if message.content.startswith("$8ball"):
+            return message.channel.send(eight_ball_answer)
 
 
 intents = discord.Intents.default()
